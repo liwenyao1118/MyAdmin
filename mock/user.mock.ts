74e5167ed6b397deb2a2aa9ddba77e83f515599a
liwenyao <975139902@qq.com>
2024-04-22 20:20:11 +0800

    refactor: 添加并配置mock

---
+++ mock/user.mock.ts
@@ -0,0 +1,47 @@
+import { defineMock } from 'vite-plugin-mock-dev-server'
+import user from './data/user.ts'
+
+export default defineMock([
+  {
+    // 登录接口
+    url: '/api/user/login',
+    method: 'POST',
+    response(req, res) {
+      //  判断当前用户列表是否存在当前用户
+      const targetUser = user.value.find((item) => {
+        if (item.username == req.body.username && item.password == req.body.password) {
+          return item
+        }
+      })
+      //   如果不存在，返回结果
+      if (!targetUser) {
+        res.end(JSON.stringify({ code: 201, data: { message: '账号或者密码不正确' } }))
+      }
+      // 如果用户存在，则返回token
+      const { token } = targetUser
+      res.end(JSON.stringify({ code: 200, data: { token } }))
+    }
+  },
+  {
+    // 获取用户信息接口
+    url: '/api/user/info',
+    method: 'GET',
+    body({headers}) {
+      // 获取当前请求携带的token，判断该用户是否存在
+      const reqToken = headers.token
+      const targetUser = user.value.find((item) => {
+        if (item.token == reqToken) {
+          return item
+        }
+      })
+      console.log(targetUser)
+
+      // 如果不存在，返回结果
+      if (!targetUser) {
+        return{ code: 201, data: { message: '获取用户信息失败' } }
+      }
+      //   如果存在，返回用户信息
+      return{ code: 200, data: { targetUser } }
+    }
+  }
+])
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+// 引入mock-dev-server插件
+import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,8 +25,31 @@ export default defineConfig({
         - dir/dir2/icon1.svg # icon-dir-dir2-icon1
       */
       symbolId: 'icon-[dir]-[name]'
+    }),
+    // 使用mock-dev-server插件
+    mockDevServerPlugin({
+      // mock文件位置(相对于根目录)
+      include: 'mock/**/*.mock.{ts,js,cjs,mjs,json,json5}',
+      // 表单相关配置
+      formidableOptions: {
+        // 配置上传资源存放目录
+        uploadDir: path.join(process.cwd(), '/uploads'),
+        // 可修改上传资源名称
+        filename: (name: any, ext: any, part: any) => {
+          return part.originalFilename!
+        }
+      }
     })
   ],
+  // 配置vite集成服务器
+  server: {
+    // 配置跨域代理
+    proxy: {
+      // 请求地址重写
+      // mock地址
+      '^/api': ''
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
